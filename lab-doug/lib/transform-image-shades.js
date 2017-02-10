d958609d4d6499a7b566a2a2d688b4b780840b43
Doug Popadince <gsmatth@icloud.com>
2017-02-10 16:19:55 -0700

    removed two console logs

--- lab-doug/lib/transform-image-shades.js
+++ lab-doug/lib/transform-image-shades.js
@@ -12,16 +12,14 @@ module.exports = function(tempBuffer, newColorTable){
     tempPixelValue.splice(0, 1, bluePixelValue);
     tempPixelValue.splice(1, 1, greenPixelValue);
     tempPixelValue.splice(2, 1, redPixelValue);
-    console.log('tempPixelValue after each loop: ', tempPixelValue);
 
     for(let ii = 0; ii < 3; ii++){
       tempPixelValue[ii] += 128;
-      console.log('changed pixel values: ', tempPixelValue[ii]);
       if(tempPixelValue[ii] > 256){
         tempPixelValue[ii] -= 256;
       }
     }
-    
+
     tempBuffer.writeUInt8(tempPixelValue[0], i);
     tempBuffer.writeUInt8(tempPixelValue[1], i + 1);
     tempBuffer.writeUInt8(tempPixelValue[2], i + 2);
